14744be677a5952fb801103892ee9bf0ea61cb8e
Sebastian Ibarguen <sebas@vana.gt>
2020-06-09 21:16:33 -0600

    Add event model and serializer. Include missing dependencies.

--- server/app/events/models.py
+++ server/app/events/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.postgres.fields import JSONField
+
+
+EVENT_TYPES = [
+    ('track', 'track'),
+    ('screen', 'screen'),
+    ('page', 'page'),
+    ('identity', 'identity'),
+]
+
+class Event(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    sent_at = models.DateTimeField()
+    timestamp = models.DateTimeField()
+
+    message_id = models.CharField(max_length=100, primary_key=True, unique=True)
+    user_id = models.CharField(max_length=100, blank=True, default='')
+    
+    event = models.CharField(max_length=100)
+    type = models.CharField(max_length=25, choices=EVENT_TYPES, default='track')
+
+    properties = JSONField()
+    context = JSONField()
+
+    data = JSONField()
+
+    class Meta:
+        ordering = ['created_at']
---
+++ server/app/events/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from events.models import Event, EVENT_TYPES
+
+
+class SnippetSerializer(serializers.Serializer):
+    message_id = serializers.CharField(max_length=100, required=True, allow_blank=False)
+    user_id = serializers.CharField(max_length=100, blank=True, default='')
+    
+    event = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    type = serializers.ChoiceField(choices=EVENT_TYPES, default='track')
+
+    properties = serializers.JSONField()
+    context = serializers.JSONField()
+
+    data = serializers.JSONField()
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return Event.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.event = validated_data.get('event', instance.event)
+        instance.type = validated_data.get('type', instance.type)
+        instance.properties = validated_data.get('properties', instance.properties)
+        instance.context = validated_data.get('context', instance.context)
+        instance.save()
+        return instance
