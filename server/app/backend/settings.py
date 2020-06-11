7db9d413f145c166e8aa3a22cfdede5bc6551421
Sebastian Ibarguen <sebas@vana.gt>
2020-06-10 21:57:08 -0600

    Fix seralizer issue. Change to postgresql

--- server/app/backend/settings.py
+++ server/app/backend/settings.py
@@ -86,9 +86,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'events',
+        'USER': 'django-user',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
--- server/app/events/serializers.py
+++ server/app/events/serializers.py
@@ -2,9 +2,9 @@ from rest_framework import serializers
 from events.models import Event, EVENT_TYPES
 
 
-class SnippetSerializer(serializers.Serializer):
+class EventSerializer(serializers.Serializer):
     message_id = serializers.CharField(max_length=100, required=True, allow_blank=False)
-    user_id = serializers.CharField(max_length=100, blank=True, default='')
+    user_id = serializers.CharField(max_length=100, allow_blank=True, default='')
     
     event = serializers.CharField(required=False, allow_blank=True, max_length=100)
     type = serializers.ChoiceField(choices=EVENT_TYPES, default='track')
@@ -16,13 +16,13 @@ class SnippetSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         """
-        Create and return a new `Snippet` instance, given the validated data.
+        Create and return a new `Event` instance, given the validated data.
         """
         return Event.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         """
-        Update and return an existing `Snippet` instance, given the validated data.
+        Update and return an existing `Event` instance, given the validated data.
         """
         instance.event = validated_data.get('event', instance.event)
         instance.type = validated_data.get('type', instance.type)
